1af9379226953f9f19f2b88e9411bc40920fdc9d
janipalli <tejajanipali@gmail.com>
2022-10-27 12:52:57 +0530

    revision

---
+++ vikram.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    # arr=list(arr)
+    # a=max(arr)
+    
+    # c=arr.count(a)
+    
+    # for i in range(c):
+    #     arr.remove(a)
+        
+    # print(max(arr))
+    print(arr)
