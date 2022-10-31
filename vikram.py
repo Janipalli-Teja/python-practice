7f05991ff9483524984a39b774d521640ae6acac
Teja <85123406+janipalli@users.noreply.github.com>
2022-10-31 19:41:45 +0530

    Update vikram.py

--- vikram.py
+++ vikram.py
@@ -1,13 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    # Write your code here
+    
+
 if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    # arr=list(arr)
-    # a=max(arr)
-    
-    # c=arr.count(a)
-    
-    # for i in range(c):
-    #     arr.remove(a)
-        
-    # print(max(arr))
-    print(arr)
+#   fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+ #  fptr.write(str(result) + '\n')
+
+ #  fptr.close()
+#  annaya above conditional statement lo comment chesina vatiki meaning enti
